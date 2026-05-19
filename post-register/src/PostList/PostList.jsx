c799a99b0b77c249ca209dfa66949dcd17771046
edoal1999-oss <edoal1999@gmail.com>
2026-05-19 17:53:58 +0900

    519menus

---
+++ post-register/src/PostList/PostList.jsx
@@ -0,0 +1,54 @@
+import Header from "../components/Header/Header";
+import TextInput from "../components/TextInput/TextInput";
+import Button from "../components/Button/Button";
+import * as s from "./styles";
+import { BiLeftArrow } from "react-icons/bi";
+import { FiChevronsLeft, FiChevronsRight } from "react-icons/fi";
+import { usePosts } from "../hooks/queries/usePosts";
+import { useState } from "react";
+
+function PostList() {
+    const [ page, setPage ] = useState(1);
+    const posts = usePosts(page, 4);
+
+    const handlePageOnClick = (e) => {
+        setPage(parseInt(e.target.value));
+    }
+
+    return (
+        <div>
+            <Header />
+            <main>
+                <article css={s.topPosting}>
+
+                </article>
+                <div css={s.searchContainver}>
+                    <TextInput />
+                    <Button>검색</Button>
+                </div>
+                <ul css={s.postListContainer}>
+                    {
+                        posts.isLoading ? <></> : posts.data.map(post => (
+                            <li key={post.id} css={s.card}>
+                                <header css={s.cardThumbnail(post.thumbnail.dataurl)}></header>
+                                <div>{post.content}</div>
+                                <footer>{post.user.fullname}</footer>
+                            </li>
+                        ))
+                    }
+                </ul>
+                <div css={s.pagination}>
+                    <Button><FiChevronsLeft/></Button>
+                        <Button value={1} onClick={handlePageOnClick}>1</Button>
+                        <Button value={2} onClick={handlePageOnClick}>2</Button>
+                        <Button value={3} onClick={handlePageOnClick}>3</Button>
+                        <Button value={4} onClick={handlePageOnClick}>4</Button>
+                        <Button value={5} onClick={handlePageOnClick}>5</Button>
+                    <Button><FiChevronsRight/></Button>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default PostList;
---
+++ post-register/src/routes/RootRoutes.jsx
@@ -0,0 +1,53 @@
+import { Routes, useNavigate } from "react-router";
+import Layout from "../components/Layout/Layout";
+import { Route } from "react-router";
+import AuthRoutes from "./AuthRoutes";
+import { useAuthentication } from "../hooks/queries/useAuthentication";
+import { useQueryClient } from "@tanstack/react-query";
+import { useLocation } from "react-router";
+import { useEffect } from "react";
+import PostList from "../PostList/PostList";
+import PostRegister from "../pages/PostRegister/PostRegister";
+import Menus from "../pages/Menus/Menus";
+
+function RootRoutes() {
+    const {pathname} = useLocation();
+    const navigate = useNavigate();
+    const authenticationQuery = useAuthentication(localStorage.getItem("accessToken"));
+
+    useEffect(() => {
+        if (!authenticationQuery.isLoading) {
+            const status = authenticationQuery.data.status;
+            if (status !== 200 && !pathname.startsWith("/auth/")) {
+                navigate("/auth/signin", {
+                    replace: true,
+                });
+            }
+            if (status === 200 && pathname.startsWith("/auth/")) {
+                navigate("/", {
+                    replace: true,
+                });
+            }
+        }
+    }, [authenticationQuery.isLoading]);
+
+    return (
+        <>
+            {
+            authenticationQuery.isLoading 
+                ? <h1>로딩중</h1> 
+                :
+                <Routes>
+                    <Route path="/" element={<PostList/>}/>
+                    <Route path="/write" element={<PostRegister />}/>
+                    <Route path="/menus" element={<Menus />}/>
+                    <Route path="/auth/*" element={<AuthRoutes />}/>
+                    <Route path="*" element={<>페이지를 찾을 수 없습니다.</>} />
+                </Routes>
+
+            }
+        </>
+    )
+}
+
+export default RootRoutes;
